50d999efdb12a216a1287b21893aff3e1f30058c
Cloudwalker23 <74190812+Cloudwalker23@users.noreply.github.com>
2021-10-25 19:33:53 +0530

    Add files via upload

---
+++ sets/sets homework Q2.py
@@ -0,0 +1,9 @@
+'''Question 2: Return a new set of identical items from a given two set
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+Expected output:
+{40, 50, 30}
+'''
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(set1.intersection(set2))
