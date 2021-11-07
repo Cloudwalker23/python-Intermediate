f032d2907ff8ec559fa6d5c0a628615c31133ee0
Disha Sehgal <74190812+Cloudwalker23@users.noreply.github.com>
2021-11-07 14:26:47 +0530

    Add files via upload

---
+++ objects and classes/class.py
@@ -0,0 +1,18 @@
+'''
+class healthy_habits:
+    L=''
+    object
+print(healthy_habits)
+'''
+#Insert a function that prints an exit message, and execute it on the p1 object:
+
+class ex:
+
+    def __init__(self,name,):
+        self.name=name
+
+    def exit(self):
+        print("goodbye",self.name," I will exit now.")
+p1=ex('simon')
+p1.exit()
+
